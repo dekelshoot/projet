838028dcdc198b12f0ee810a31e813506eace659
dekelshoot <69307490+dekelshoot@users.noreply.github.com>
2022-03-23 06:54:28 -0700

    recherches de corine

---
+++ app.js
@@ -0,0 +1,35 @@
+const video = document.getElementById('video')
+
+//var constraints = {audio: true, video: {width:120, height:720}};
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo)
+
+function startVideo(){
+    navigator.getUserMedia(
+        {video: {}},
+        stream => video.srcObject = stream, 
+        err => console.error(err)
+    )
+}
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas)
+    const displaySize = {width: video.width, height: video.height}
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).
+        withFaceLandmarks().withFaceExpressions()
+        console.log(detections)
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections) //Pour cadrer le visage
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections) //Pour cadrer les éléments du visage(nez, bouche,yeux)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections) // Pour connaitre l'expression que le visage dégage
+    }, 100)
+})
